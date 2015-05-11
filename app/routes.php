49c6a80be4e4235ae9f005b0f7524d5464886315
hsosagm <intelpcventas@hotmail.com>
2015-05-10 18:22:36 -0600

    test commit

--- app/routes.php
+++ app/routes.php
@@ -339,6 +339,7 @@ Route::get('dbseed', 'LoadDataController@index');
 Route::get('timetest', function() 
 {
     echo 'ok';
+    echo 'ok';
     $start = date('Y/m/d H:i:s');
     $start = round(microtime(true) * 1000);
 
