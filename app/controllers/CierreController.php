435bc634b2cb06afece73c4c43f3daeafec2b67f
hsosagm <intelpcventas@hotmail.com>
2015-07-02 18:08:35 -0600

    fix commit

--- app/controllers/CierreController.php
+++ app/controllers/CierreController.php
@@ -23,7 +23,7 @@ class CierreController extends \BaseController {
         $Query = DB::table('metodo_pago')
         ->select(DB::raw("metodo_pago.descripcion as descripcion, sum({$campo}) as total"))
         ->join($tabla,"{$tabla}.metodo_pago_id" , "=" , "metodo_pago.id")
-        ->whereRaw("DATE_FORMAT({$tabla}.created_at, '%Y-%m-%d')= DATE_FORMAT('{$fecha}', '%Y-%m-%d')")
+        ->whereRaw("DATE_FORMAT({$tabla}.created_at, '%Y-%m-%d')= DATE_FORMAT({$fecha}, '%Y-%m-%d')")
         ->groupBy('metodo_pago.id')->get();
 
         return $this->llenar_arreglo($Query);
