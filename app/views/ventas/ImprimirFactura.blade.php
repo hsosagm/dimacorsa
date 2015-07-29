b431e9aa1e7a1a1e7988a6908b6a681b82c4cf2d
hsosagm <intelpcventas@hotmail.com>
2015-07-29 08:32:57 -0600

    fix

--- app/views/ventas/ImprimirFactura.blade.php
+++ app/views/ventas/ImprimirFactura.blade.php
@@ -1,29 +1,28 @@
 <html>
-	<head>
-		<style type="text/css">
+<head>
+	<style type="text/css">
 
-			body
-			{
-				margin-top: 35px;
-				margin-left: 10px;
-			}
+		body
+		{
+			margin-top: 35px;
+			margin-left: 10px;
+		}
 
-			body, td
-			{
-				font-size: 9pt;
-				font-family: "monospace";
-			}
+		body, td
+		{
+			font-size: 9pt;
+			font-family: "monospace";
+		}
 
-			#factura_detalle {
-				display: block;
-				height: 200px;
-				padding-top: 10px;
-				padding-bottom: 70px;
-			}
-		</style>
-	</head>
+		#factura_detalle {
+			display: block;
+			height: 200px;
+			padding-top: 10px;
+			padding-bottom: 70px;
+		}
+	</style>
+</head>
 <body>
-
 
 	<table>
 		<tr height="25"> 
@@ -40,30 +39,35 @@
 	</table>
 
 	<div id="factura_detalle">
-		<table>
+		<table face="monospace" border="1px">
 			<tbody>
 				<?php $total = 0; ?>
 				@foreach($venta->detalle_venta as $key => $dt)
-				<tr style="line-height:10px;">
-					<td width="50">  {{ $dt->cantidad }} </td>	
-					<td width="375"> {{ $dt->producto->descripcion}} </td>
-					<td width="65" align="right">
+				<tr height="3cm">
+					<td valign="top" width="50">  {{ $dt->cantidad }} </td>
+
+
+				    <?php $descripcion = str_replace(" ", "&nbsp;", $dt->producto->descripcion); ?>
+				    <?php $descripcion = str_replace("’", "'", $descripcion); ?>
+				    <?php $descripcion = str_replace("-", "&#8209;", $descripcion); ?>
+
+					<td valign="top" width="375"> {{ $descripcion }} </td>
+					<td valign="top" width="65" align="right">
 						{{ f_num::get($dt->precio) }}
 					</td>
-					<td width="65" align="right">
+					<td valign="top" width="65" align="right">
 						{{ f_num::get($dt->cantidad * $dt->precio)}}
 					</td>	
 				</tr>
 				<?php $total = $total +($dt->cantidad * $dt->precio); ?>
 				@endforeach
 			</tbody>
-
 		</table>
 	</div>
 
 	<table>
 		<tr>
-		    <td width="60"></td>	
+			<td width="60"></td>	
 			<td width="430">Veintiseis mil seiscientos cincuenta y cinco quetzales con 15/100 centavos</td>
 			<td width="65" align="right">90,909.99</td>
 		</tr>
