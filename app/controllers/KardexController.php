2fa04182500858d05adac3fb8d537da5ff7592c2
Nelug - ASUS <nelug.madrid@gmail.com>
2015-08-03 14:00:23 -0600

    fix commit

--- app/controllers/KardexController.php
+++ app/controllers/KardexController.php
@@ -22,8 +22,17 @@ class KardexController extends \BaseController {
         $Join = "JOIN users ON (users.id = kardex.user_id) ";
         $Join .= "JOIN kardex_transaccion ON (kardex_transaccion.id = kardex_transaccion_id) ";
 
-        $where  = " kardex.tienda_id =".Auth::user()->tienda_id;
+        $where = " DATE_FORMAT(kardex.created_at, '%Y-%m') = DATE_FORMAT(current_date, '%Y-%m') ";
+
+        if (Input::get('fecha_inicial') != null && Input::get('fecha_final') != null) 
+        {
+            $where  = "DATE_FORMAT(kardex.created_at, '%Y-%m-%d') >= DATE_FORMAT('".Input::get('fecha_inicial')."', '%Y-%m-%d')";
+            $where .= " AND DATE_FORMAT(kardex.created_at, '%Y-%m-%d') <= DATE_FORMAT('".Input::get('fecha_final')."', '%Y-%m-%d')";
+        }
+
+        $where .= " AND kardex.tienda_id =".Auth::user()->tienda_id;
         $where .= " AND kardex.producto_id =".Input::get('producto_id');
+        $where .= " ORDER BY kardex.created_at";
 
         $kardex = SST::get($table, $columns, $Search_columns, $Join, $where );
 
