2633947b37ac35a88ccf0e622e723dfda5ab77fe
Nelug-Asus <nelug@tio-gt.com>
2015-08-18 10:41:45 -0600

    fix commit

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel',
+			'database'  => 'pruebas',
 			'username'  => 'clickdb',
 			'password'  => 'RjAzsMSBzLzPcxWM',
 			'charset'   => 'utf8',
