3c6d17df1b6e520f89978ecfd3a1bc17297efb86
Leonel Madrid <nelug@tio-gt.com>
2015-09-01 12:04:38 -0600

    fix commit

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'bdinicial',
+			'database'  => 'laravel',
 			'username'  => 'clickdb',
 			'password'  => 'RjAzsMSBzLzPcxWM',
 			'charset'   => 'utf8',
