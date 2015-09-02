baa8b79714dc859f8e09ea1fb78e7eb1f37159a3
Leonel Madrid <nelug@tio-gt.com>
2015-09-02 10:48:32 -0600

    fix commit

--- app/views/descargas/imprimir.blade.php
+++ app/views/descargas/imprimir.blade.php
@@ -1,14 +1,15 @@
-<div width="100%">
-    <h1> Constancia de Descarga : {{ $descarga->id }} </h1>
-    <table width="100%" class="table table-condensed">
-        <tr>
-            <th width="15%">Cantidad</th>
-            <th width="55%">Descripcion</th>
-            <th width="15%">Precio</th>
-            <th width="15%">Total</th>
-        </tr>
-        <?php $total = 0; ?>
-        @foreach($descarga->detalle_descarga as $key => $dt)
+<div width="Impresiones">
+    <div width="ImpresionesBody">
+        <h1> Constancia de Descarga : {{ $descarga->id }} </h1>
+        <table width="100%" class="table table-condensed">
+            <tr>
+                <th width="15%">Cantidad</th>
+                <th width="55%">Descripcion</th>
+                <th width="15%">Precio</th>
+                <th width="15%">Total</th>
+            </tr>
+            <?php $total = 0; ?>
+            @foreach($descarga->detalle_descarga as $key => $dt)
             <tr height="1"  style="font-size:12px; ">
                 <td width="15%"> {{ $dt->cantidad }} </td>   
                 <td width="55%"> {{ $dt->producto->descripcion}} </td>
@@ -16,32 +17,33 @@
                 <td width="15%" style="text-align:right; padding-right:15px">{{ f_num::get($dt->cantidad * $dt->precio)}} </td>   
             </tr>
             <?php $total = $total +($dt->cantidad * $dt->precio); ?>
-        @endforeach
-        <tfoot height="15">
+            @endforeach
+            <tfoot height="15">
+                <tr>
+                    <td style="font-size:12px; " colspan="3"> Total: </td>
+                    <td style="text-align:right; padding-right:15px"> {{f_num::get($total)}} </td>
+                </tr>
+            </tfoot>
+        </table>
+        <br><br>
+        <table width="100%" class="table table-bordered">
             <tr>
-                <td style="font-size:12px; " colspan="3"> Total: </td>
-                <td style="text-align:right; padding-right:15px"> {{f_num::get($total)}} </td>
+                <td width="33%" align="center"> Usuario :  </td>
+                <td width="33%" align="center"> Fecha de  creacion : </td>
+                <td width="33%" align="center"> Fecha de impresion : </td>
             </tr>
-        </tfoot>
-    </table>
-    <br><br>
-    <table width="100%" class="table table-bordered">
-        <tr>
-            <td width="33%" align="center"> Usuario :  </td>
-            <td width="33%" align="center"> Fecha de  creacion : </td>
-            <td width="33%" align="center"> Fecha de impresion : </td>
-        </tr>
-        <tr>
-            <td width="33%" align="center"> {{ Auth::user()->nombre .' '.Auth::user()->apellido }} </td>
-            <td width="33%" align="center"> {{ $descarga->created_at }}</td>
-            <td width="33%" align="center"> {{ date('Y-m-d h:i:s')}} </td>
-        </tr>
-    </table>
-    <br>
-    Nota:
-    <table width="100%" class="table table-bordered">
-        <tr> <td width="100%">{{ $descarga->descripcion }}</td>  </tr>
-        <tr> <td width="100%">-</td> </tr>
-        <tr> <td width="100%">-</td> </tr>
-    </table>
+            <tr>
+                <td width="33%" align="center"> {{ Auth::user()->nombre .' '.Auth::user()->apellido }} </td>
+                <td width="33%" align="center"> {{ $descarga->created_at }}</td>
+                <td width="33%" align="center"> {{ date('Y-m-d h:i:s')}} </td>
+            </tr>
+        </table>
+        <br>
+        Nota:
+        <table width="100%" class="table table-bordered">
+            <tr> <td width="100%">{{ $descarga->descripcion }}</td>  </tr>
+            <tr> <td width="100%">-</td> </tr>
+            <tr> <td width="100%">-</td> </tr>
+        </table>
+    </div>
 </div>
--- app/views/proveedor/ImprimirAbono.blade.php
+++ app/views/proveedor/ImprimirAbono.blade.php
@@ -1,80 +1,82 @@
-<div width="100%">
-	<table width="100%" class="table table-bordered">
-		<tr>
-			<td> Nombre :</td>
-			<td> {{ $abono->proveedor->nombre }} </td>
-			<td> Direccion :</td>
-			<td> {{ $abono->proveedor->direccion }} </td>
-		</tr>
-		<tr>
-			<td> Nit :</td>
-			<td> {{ $abono->proveedor->nit }} </td>
-			<td> Telefono :</td>
-			<td> {{ $abono->proveedor->telefono }} </td>
-		</tr>
-	</table>
-	<br>
-	<table width="100%" class="table table-bordered">
-		<tr>
-			<td> Metodo Pago :</td>
-			<td> {{ $abono->metodoPago->descripcion }} </td>
-			<td> Fecha :</td>
-			<td> {{ $abono->created_at}} </td>
-		</tr>
-		<tr>
-			<td> Usuario :</td>
-			<td colspan="3"> {{ $abono->user->nombre .' '. $abono->user->apellido}} </td>
-		</tr>
-		<tr>
-			<td>Observaciones</td>
-			<td colspan="3"> {{ $abono->observaciones }} </td>
-		</tr>
-	</table>
-	<table width="100%" class="table table-bordered">
-		<tr>
-			<td> Saldo ala fecha:</td>
-			<td> Q {{ f_num::get($saldo->total) }} </td>
-			<td> Monto total abonado :</td>
-			<td> Q {{ f_num::get($abono->monto) }} </td>
-		</tr>
-	</table>
-	<table class="table table-condensed" width="100%">
-		<thead>
-			<tr class="">
-				<th>No. Compra</th>
-				<th>Total</th>
-				<th>Monto Abonado </th>
-				<th>Saldo Anterior</th>
-				<th>Nuevo Saldo</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($detalle as $key => $dt)
-			<?php
-			$total = f_num::get($dt->total);
-			$monto = f_num::get($dt->monto);
-
-			$abonos = DetalleAbonosCompra::select(DB::raw('sum(monto) as total'))
-			->where('compra_id','=',$dt->compra_id)
-			->where('created_at','<',$dt->fecha)->first();
-
-			$pagos = PagosCompra::select(DB::raw('sum(monto) as total'))
-			->where('compra_id','=',$dt->compra_id)
-			->where('metodo_pago_id','!=', 2)
-			->where('created_at','<',$dt->fecha)->first();
-
-			$saldo_ant = $dt->total - ($abonos->total + $pagos->total);
-			$saldo_anterior = f_num::get($saldo_ant);
-			$saldo =  f_num::get(($saldo_ant - $dt->monto));
-			?>
+<div width="Impresiones">
+	<div width="ImpresionesBody">
+		<table width="100%" class="table table-bordered">
 			<tr>
-				<td>{{$dt->compra_id}}</td>
-				<td class="align_right">{{$total}}</td>
-				<td class="align_right">{{$monto}}</td>
-				<td class="align_right">{{$saldo_anterior}}</td>
-				<td class="align_right">{{$saldo}}</td>
+				<td> Nombre :</td>
+				<td> {{ $abono->proveedor->nombre }} </td>
+				<td> Direccion :</td>
+				<td> {{ $abono->proveedor->direccion }} </td>
 			</tr>
-			@endforeach
-		</tbody>
-	</table>
+			<tr>
+				<td> Nit :</td>
+				<td> {{ $abono->proveedor->nit }} </td>
+				<td> Telefono :</td>
+				<td> {{ $abono->proveedor->telefono }} </td>
+			</tr>
+		</table>
+		<br>
+		<table width="100%" class="table table-bordered">
+			<tr>
+				<td> Metodo Pago :</td>
+				<td> {{ $abono->metodoPago->descripcion }} </td>
+				<td> Fecha :</td>
+				<td> {{ $abono->created_at}} </td>
+			</tr>
+			<tr>
+				<td> Usuario :</td>
+				<td colspan="3"> {{ $abono->user->nombre .' '. $abono->user->apellido}} </td>
+			</tr>
+			<tr>
+				<td>Observaciones</td>
+				<td colspan="3"> {{ $abono->observaciones }} </td>
+			</tr>
+		</table>
+		<table width="100%" class="table table-bordered">
+			<tr>
+				<td> Saldo ala fecha:</td>
+				<td> Q {{ f_num::get($saldo->total) }} </td>
+				<td> Monto total abonado :</td>
+				<td> Q {{ f_num::get($abono->monto) }} </td>
+			</tr>
+		</table>
+		<table class="table table-condensed" width="100%">
+			<thead>
+				<tr class="">
+					<th>No. Compra</th>
+					<th>Total</th>
+					<th>Monto Abonado </th>
+					<th>Saldo Anterior</th>
+					<th>Nuevo Saldo</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($detalle as $key => $dt)
+				<?php
+				$total = f_num::get($dt->total);
+				$monto = f_num::get($dt->monto);
+
+				$abonos = DetalleAbonosCompra::select(DB::raw('sum(monto) as total'))
+				->where('compra_id','=',$dt->compra_id)
+				->where('created_at','<',$dt->fecha)->first();
+
+				$pagos = PagosCompra::select(DB::raw('sum(monto) as total'))
+				->where('compra_id','=',$dt->compra_id)
+				->where('metodo_pago_id','!=', 2)
+				->where('created_at','<',$dt->fecha)->first();
+
+				$saldo_ant = $dt->total - ($abonos->total + $pagos->total);
+				$saldo_anterior = f_num::get($saldo_ant);
+				$saldo =  f_num::get(($saldo_ant - $dt->monto));
+				?>
+				<tr>
+					<td>{{$dt->compra_id}}</td>
+					<td class="align_right">{{$total}}</td>
+					<td class="align_right">{{$monto}}</td>
+					<td class="align_right">{{$saldo_anterior}}</td>
+					<td class="align_right">{{$saldo}}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
 </div>
