2d213a5867b610cde7d4833aa8e1891baa2007bc
Leonel Madrid <nelug@tio-gt.com>
2015-09-02 12:45:19 -0600

    fix commit

--- app/views/user_consulta/VentasDelDia.blade.php
+++ app/views/user_consulta/VentasDelDia.blade.php
@@ -15,17 +15,17 @@ $(document).ready(function() {
         },
         
         "aoColumnDefs": [
-            {"sClass": "widthM",                                        "sTitle": "Fecha",                        "aTargets": [0]},
-            {"sClass": "widthM",                                        "sTitle": "Vendedor",                     "aTargets": [1]},
-            {"sClass": "widthM",                                        "sTitle": "Cliente",                      "aTargets": [2]},
-            {"sClass": "widthS right formato_precio",                   "sTitle": "Total",                        "aTargets": [3]},
-            {"sClass": "widthS right formato_precio",                   "sTitle": "Saldo",                        "aTargets": [4]},
-            {"bVisible": false,                                                                                   "aTargets": [5]},
-            {"sClass": "widthS icons",  "orderable": false,             "sTitle": "",                             "aTargets": [6],
-                "mRender": function() {
+            {"sClass": "widthM",                                        "sTitle": "Fecha",   "aTargets": [0]},
+            {"sClass": "widthM",                                        "sTitle": "Vendedor","aTargets": [1]},
+            {"sClass": "widthM",                                        "sTitle": "Cliente", "aTargets": [2]},
+            {"sClass": "widthS right formato_precio",                   "sTitle": "Total",   "aTargets": [3]},
+            {"sClass": "widthS right formato_precio",                   "sTitle": "Saldo",   "aTargets": [4]},
+            {"bVisible": false,                                                              "aTargets": [5]},
+            {"sClass": "widthS icons center", "orderable": false,             "sTitle": "",        "aTargets": [6],
+                "mRender": function(  data, type, full ) {
                     $v  = '<a href="javascript:void(0);" title="Ver detalle" onclick="showSalesDetail(this)" class="fa fa-plus-square show_detail font14">';
                     $v += '<a href="javascript:void(0);" title="Abrir venta" onclick="openSale(this)" class="fa fa-pencil-square-o font14" style="padding-left:10px">';
-                    $v += '<a href="javascript:void(0);" title="Imprimir Factura" onclick="ImprimirFacturaVenta_dt(this,{{Auth::user()->id}})" class="fa fa-print font14" style="padding-left:10px">';
+                    $v += '<a href="javascript:void(0);" title="Imprimir Factura" onclick="printInvoice(this, '+full.DT_RowId+', '+"'"+'{{@$factura->impresora}}'+"'"+')" class="fa fa-print font14" style="padding-left:10px">';
                     $v += '<a href="javascript:void(0);" title="Imprimir Garantia" onclick="ImprimirGarantiaVenta_dt(this,{{Auth::user()->id}})" class="fa fa-file-o font14" style="padding-left:10px">';
 
                     return $v;
