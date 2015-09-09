d4a49ae59aacecd2dbe102b100d233517a98954e
hsosagm <intelpcventas@hotmail.com>
2015-09-09 15:54:40 -0600

    fix and change to gulp

--- app/views/partials/head.blade.php
+++ app/views/partials/head.blade.php
@@ -1,12 +1,9 @@
 <head>
 	<title>{{$tienda->nombre}}| Web App</title>
 	{{ HTML::style('css/main.css'); }}
+	{{ HTML::style('css/custom.css'); }}
 	<link href="css/themes/default.theme.css" rel="stylesheet" id="theme">
 	<link href="calendar/themes/classic.css" rel="stylesheet">
 	<link href="calendar/themes/classic.date.css" rel="stylesheet">
 	<script type="text/javascript" src="js/qz/deployJava.js"></script>
 </head>
-
-
-
-{{-- <div id="customjs"></div> --}}
