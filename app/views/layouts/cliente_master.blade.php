48fa1bfffdaf4675a463a27014bb7c0510848542
hsosagm <intelpcventas@hotmail.com>
2015-09-11 18:25:25 -0600

    fix commit

--- app/views/layouts/cliente_master.blade.php
+++ app/views/layouts/cliente_master.blade.php
@@ -46,19 +46,8 @@
     <i class="fa fa-angle-up"></i>
 </div>
 
-<div class="graficas_auxiliar" style="display:none"></div>
-
-<script src="js/vue.min.js"></script>
 <script src="js/main.js"></script>
 <script src="js/cliente.js"></script>
-<script src="calendar/picker.js"></script>
-<script src="calendar/picker.date.js"></script>
-<script src="calendar/translations/es_ES.js"></script>
-<script type="text/javascript" src="js/qz/deployJava.js"></script>
-<script type="text/javascript" src="js/qz/PluginDetect.js"></script>
-<script type="text/javascript" src="js/qz/qz.js"></script>
-<script type="text/javascript" src="js/qz/html2canvas.js"></script>
-<script type="text/javascript" src="js/qz/jquery.plugin.html2canvas.js"></script>
 
 <script>
     $(document).ready(function(){
@@ -94,8 +83,7 @@
         }
 
     }); 
-</script>
-<script>
+
 
     $('#customer_search').autocomplete({
         serviceUrl: '/user/cliente/search',
@@ -131,9 +119,8 @@
 
     $('#date-input').datepicker();
 
-</script> 
-<canvas id="hidden_screenshot" style="display:none;"></canvas>
-<canvas id="barcode" style="display:none;"></canvas>
+</script>
+
 </body>
 
 </html>
