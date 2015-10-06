9725f4e0071a56ddf8974be9f647fc31671a395a
Nelug <nelug@tio-gt.com>
2015-10-06 16:33:47 -0600

    config email

--- app/funciones/ConfigEmail.php
+++ app/funciones/ConfigEmail.php
@@ -4,61 +4,33 @@ class ConfigEmail {
 
 	public static function driver () {
 		return 'smtp';
-	} 
+	}
 
 	public static function host () {
 		return 'smtp.sendgrid.net';
-	} 
+	}
 
 	public static function port () {
 		return 587;
-	} 
+	}
 
 	public static function address () {
-		return 'appReport@notificacion.com';
-	} 
+		return  getenv('MAIL_ADDRESS');
+	}
 
 	public static function name () {
-		return 'Notificacion';
-	} 
+		return  getenv('MAIL_NAME');
+	}
 
 	public static function encryption () {
 		return 'tls';
-	} 
+	}
 
 	public static function username () {
-		return 'nelug';
+		return getenv('MAIL_USERNAME');
 	}
 
 	public static function password() {
-		return preg_replace('/[^A-Za-z0-9-.]/', '',ConfigEmail::_password());
+		return getenv('MAIL_PASSWORD');
 	}
-
-	public static function _password() {
-		return ConfigEmail::decrypt('lpvBq6CiyJaVrJ2PsamUrJiU2KmPnqvSyKmPnqvaqciPnqvTyJaUrNLBlpvUy5iUmsiPnqvKqciPnqvYqciP','leonel');
-	}
-
-	public static function encrypt($string, $key) {
-		$result = '';
-		for($i=0; $i<strlen($string); $i++) {
-			$char = substr($string, $i, 1);
-			$keychar = substr($key, ($i % strlen($key))-1, 1);
-			$char = chr(ord($char)+ord($keychar));
-			$result.=$char;
-		}
-		return base64_encode($result);
-	}
-
-	public static function decrypt($string, $key) {
-		$result = '';
-		$string = base64_decode($string);
-		for($i=0; $i<strlen($string); $i++) {
-			$char = substr($string, $i, 1);
-			$keychar = substr($key, ($i % strlen($key))-1, 1);
-			$char = chr(ord($char)-ord($keychar));
-			$result.=$char;
-		}
-		return $result;
-	}
-
 }
