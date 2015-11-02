3b6f02106a7bd77a785c7ed6271719912599a327
hsosagm <intelpcventas@hotmail.com>
2015-11-02 05:56:14 -0600

    add ajuste inventario link

--- app/views/partials/Controles/operaciones.blade.php
+++ app/views/partials/Controles/operaciones.blade.php
@@ -9,6 +9,7 @@
 		<li> <a href="javascript:void(0)" onclick="f_coti_op()"><i class="fa fa-list-ol"></i> Cotizacion</a> </li>
 		<li> <a href="javascript:void(0)" onclick="fopen_descarga();"><i class="fa fa-download"></i> Descarga</a></li>
 		<li> <a href="javascript:void(0)" onclick="fopen_traslado();"><i class="fa fa-exchange"></i> Traslado</a> </li>
+		<li><a href="javascript:void(0);" onclick="inventario()">Ajuste de inventario</a></li>
 		<li> <a href="javascript:void(0)" onclick="getActualizarPagosCompraFinalizada(this, 2928);"><i class="fa fa-exchange"></i>prueba</a> </li>
 	</ul>
 </li>
