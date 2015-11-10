ac56b28e0cbfde53e58eb795d5e54a12e0bc6600
hsosagm <intelpcventas@hotmail.com>
2015-11-10 16:23:57 -0600

    start db backup command

---
+++ app/commands/DatabaseBackupCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DatabaseBackupCommand extends Command {
+
+	protected $name = 'backup:db';
+
+	protected $description = 'Buckup all databases.';
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function fire()
+	{
+        $this->info( "Todo generado con exito" );
+	}
+}
