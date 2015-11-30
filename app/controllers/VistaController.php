a25d00df9ce49ae0fff99ddb1cf5ad39809960ac
nelug <leonel.madrid@hotmail.com>
2015-11-30 10:41:27 -0600

    fix commit

--- app/controllers/VistaController.php
+++ app/controllers/VistaController.php
@@ -2,24 +2,4 @@
 
 class VistaController extends \BaseController {
 
-	public function cambiarVistaPuntoDeVenta() {
-		$user = User::find(Auth::user()->id);
-		$user->vista = "User";
-		$user->caja_id = 0;
-		$user->save();
-
-	}
-
-	public function cambiarVistaAdministardor() {
-		$user = User::find(Auth::user()->id);
-		$user->vista = "Admin";
-		$user->save();
-	}
-
-	public function cambiarVistaPropietario() {
-		$user = User::find(Auth::user()->id);
-		$user->vista = "Owner";
-		$user->save();
-	}
-
 }
