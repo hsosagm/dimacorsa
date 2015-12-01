852e721fddfe134297f1a81f21f4de63eefc3ee3
nelug <leonel.madrid@hotmail.com>
2015-12-01 09:41:30 -0600

    fix commit

--- app/routes.php
+++ app/routes.php
@@ -713,6 +713,7 @@
     Route::get('/test', function()
     {     
         echo json_encode(Producto::find(1005585));
+        echo json_encode(Existencia::whereProductoId(1005585));
         //Existencia::whereTiendaId(1)->update(array('existencia_real'=>null, 'ajuste' => null, 'status' => 0, 'user_id' => null));
         // $info = new InformeGeneralController;
         // return $info->procesarInformeDelDia();
