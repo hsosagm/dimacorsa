0d2c364e470f3d4c4b2071136ee566f0422f6b20
hsosagm <intelpcventas@hotmail.com>
2016-01-04 07:18:24 -0600

    fix commit

--- app/views/partials/Controles/operaciones.blade.php
+++ app/views/partials/Controles/operaciones.blade.php
@@ -9,6 +9,6 @@
 		<li> <a href="javascript:void(0)" onclick="f_coti_op()"><i class="fa fa-list-ol"></i> Cotizacion</a> </li>
 		<li> <a href="javascript:void(0)" onclick="fopen_descarga();"><i class="fa fa-download"></i> Descarga</a></li>
 		<li> <a href="javascript:void(0)" onclick="fopen_traslado();"><i class="fa fa-exchange"></i> Traslado</a> </li>
-		{{-- <li><a href="javascript:void(0);" onclick="inventario()">Ajuste de inventario</a></li> --}}
+		<li><a href="javascript:void(0);" onclick="inventario()">Ajuste de inventario</a></li>
 	</ul>
 </li>
