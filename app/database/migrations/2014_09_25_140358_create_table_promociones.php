41ec51efceb6df964d11e75dd09f86b7d41fe85e
nelug <leonel.madrid@hotmail.com>
2016-02-22 15:51:48 -0600

    fix commit

--- app/database/migrations/2014_09_25_140358_create_table_promociones.php
+++ app/database/migrations/2014_09_25_140358_create_table_promociones.php
@@ -12,7 +12,7 @@ class CreateTablePromociones extends Migration {
 			$table->increments('id');
 			$table->integer('producto_id')->unsigned();
 			$table->integer('precio_venta_id')->unsigned();
-			$table->decimal('p_promocion', 8, 25);
+			$table->decimal('p_promocion', 8, 5);
 			$table->date('fecha_inicio');
 			$table->date('fecha_fin');
 			$table->timestamps();
