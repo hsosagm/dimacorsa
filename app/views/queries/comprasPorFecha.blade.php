0ca579f770a9a2d7df12e6d324a411f2cfe71b85
nelug <leonel.madrid@hotmail.com>
2016-02-23 13:59:59 -0600

    fix commit

--- app/views/queries/comprasPorFecha.blade.php
+++ app/views/queries/comprasPorFecha.blade.php
@@ -35,6 +35,7 @@
                     }
                 }, 
             ],
+            "order": [[ 1, "desc" ]],
             "fnDrawCallback": function( oSettings ) {
                 $( ".DTTT" ).html("");
                 $("td[class*='formato_precio']").each(function() {
