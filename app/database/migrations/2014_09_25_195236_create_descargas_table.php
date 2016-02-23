e4ecee9a92ae8df2978f42f6ad8b0cc9afd431a5
nelug <leonel.madrid@hotmail.com>
2016-02-23 10:20:04 -0600

    fix commit

--- app/database/migrations/2014_09_25_195236_create_descargas_table.php
+++ app/database/migrations/2014_09_25_195236_create_descargas_table.php
@@ -25,13 +25,12 @@ class CreateDescargasTable extends Migration {
 			$table->increments('id');
 			$table->integer('descarga_id')->unsigned();
 			$table->integer('producto_id')->unsigned();
-			$table->decimal('cantidad', 8, 5);
+			$table->decimal('cantidad', 8, 2);
 			$table->decimal('precio', 8, 5);
 			$table->text('serials');
 			$table->timestamps();
 			$table->foreign('descarga_id')->references('id')->on('descargas')->onDelete('cascade')->onUpdate('cascade');
 			$table->foreign('producto_id')->references('id')->on('productos')->onDelete('restrict')->onUpdate('cascade');
-
 		});
 	}
 
