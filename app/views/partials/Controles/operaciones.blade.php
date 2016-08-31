fdb3913abf1d53a2c2e9bc3c5360e361e261596d
hsosagm <intelpcventas@hotmail.com>
2016-08-31 06:57:32 -0600

    show ajuste de inventario

--- app/views/partials/Controles/operaciones.blade.php
+++ app/views/partials/Controles/operaciones.blade.php
@@ -9,7 +9,13 @@
 		<li><a href="javascript:void(0)" onclick="f_coti_op();"><i class="fa fa-list-ol"></i> Cotizacion</a> </li>
 		<li><a href="javascript:void(0)" onclick="fopen_descarga();"><i class="fa fa-download"></i> Descarga</a></li>
 		<li><a href="javascript:void(0)" onclick="fopen_traslado();"><i class="fa fa-exchange"></i> Traslado</a></li>
-		<li><a href="javascript:void(0)" onclick="fopen_kit();"><i class="fa fa-exchange"></i> Kit</a></li>
+		<li><a href="javascript:void(0)" onclick="fopen_kit();"><i class="fa fa-exchange"></i> Crear combo</a></li>
+		<li><a href="javascript:void(0)" onclick="historial_kits();"><i class="fa fa-exchange"></i> Historial de combos</a></li>
+
+		@if(Auth::user()->hasRole("Owner"))
+            <li id="users_list"><a href="javascript:void(0);"><i class="fa fa-users"></i> Usuarios</a></li>
+		@endif
+
 		<li><a href="javascript:void(0);" onclick="inventario();">Ajuste de inventario</a></li>
 	</ul>
 </li>
